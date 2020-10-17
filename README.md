4e5270545fceee0d25654797e6bf119aa90bbdcc
Jürg Danuser <jdanuser@proffix.net>
2020-10-17 11:51:41 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+SensorDataReporter
+==================
+
+Software welches Sensordaten an die [SensorAPI](https://github.com/tschuegge/SensorAPI) überträgt.
+
+Inbetriebname
+-------------
+Das Programm wurde mit [VisualStudio Code](https://code.visualstudio.com) erstellt.
+1. Installation [NodeJS](https://nodejs.org)
+2. Installation Angular CLI: `npm install typescript -g`
+3. Abhängigkeiten installieren: `npm install` (im Projekt-Verzeichnis)
+4. Software aufrufen: `npm start`
+
+Debugging
+---------
+Der Debugger in VisualStudio Code kann mit [F5] oder direkt im VisualStudio Code gestartet werden.
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import SysInfo from "systeminformation";
+import HttpClient from "got";
+
+class Worker {
+  static async doWork(): Promise<void> {
+    console.log("start...");
+
+    /***
+     * 1. Sensorwerte lesen
+     * Lese die folgenden Werte aus dem System aus:
+     * - Freier Arbeitsspeicher
+     * - Anzahl laufende Prozesse
+     * - Gesamttemperatur der CPU
+     * - Restkapazität der Batterie in Prozent
+     * 
+     * Speichere diese Werte in einzelnen Variablen. Verwende den "async/await style".
+     * Die Programmbibliothek "systeminformation" (zum Auslesen der Informationen) wurde als "SysInfo" bereits importiert.
+     * Doku von "systeminformation": https://systeminformation.io
+     * 
+     * Beispiel, wie der Hersteller der CPU in die Konsole ausgegeben werden kann:
+     * let cpuInfo = await SysInfo.cpu();
+     * console.log(cpuInfo.manufacturer);
+     */
+
+
+
+
+
+    /***
+     * 2. Werte auf SensorAPI übertragen (die Sensoren müssen vorgängig angelegt werden)
+     * Doku von "SensorAPI": https://github.com/tschuegge/SensorAPI
+     * 
+     * Die Programmbibliothek "got" (zum Kommunizieren mit der REST API) wurde als "HttpClient" bereits importiert.
+     * Doku von "got": https://github.com/sindresorhus/got
+     * 
+     * Beispiel wie ein HTTP-Request (PUT) abgesendet werden kann:
+     * let payload = { id: null, name: "Test" };
+     * HttpClient.post("http://demoapi.invalid.com/api", { json: payload });
+     */
+
+
+
+
+    console.log("...end");
+  }
+}
+
+setTimeout( // ersetzen durch "setInterval" um die Werte regelmässig zu übertragen
+  () => {
+    Worker.doWork();
+  },
+  1000 // Intervall in dem Werte auf die SensorAPI gesendet werden
+);
+
